6f8e6e653939cfcf873b15ab268cf1fc20986e7c
Đỗ Thanh Đào <dothanhdao37577@gmail.com>
2024-12-17 12:55:03 +0700

    khởi tạo dự án

---
+++ DoAn_DieuKhienMayTinh/src/doan_dieukhienmaytinh/DoAn_DieuKhienMayTinh.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package doan_dieukhienmaytinh;
+
+/**
+ *
+ * @author latitude 5420
+ */
+public class DoAn_DieuKhienMayTinh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
